6670010b6f463fd48a978b4c16f6e6236ffdb619
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2023-04-30 15:46:58 +0000

    added a wand for player

--- ItsElementary/Assets/Player/Scripts/PlayerShooting.cs
+++ ItsElementary/Assets/Player/Scripts/PlayerShooting.cs
@@ -9,6 +9,12 @@ public class PlayerShooting : MonoBehaviour
     public float projectileSpeed = 50;
     private Vector2 lookDirection;
     private float lookAngle;
+    private float timeSinceLastShot;
+
+    void Start()
+    {
+        timeSinceLastShot = Time.realtimeSinceStartup;
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,14 +23,21 @@ public class PlayerShooting : MonoBehaviour
         lookAngle = Mathf.Atan2(lookDirection.y - transform.position.y, lookDirection.x - transform.position.x) * Mathf.Rad2Deg;
         Debug.Log(lookDirection.x +","+transform.position.x);
         firePoint.rotation = Quaternion.Euler(0, 0, lookAngle);
-
-        if (Input.GetMouseButton(0))
+        bool elementalAttack = Input.GetMouseButton(0);
+        bool basicAttack = Input.GetMouseButton(1);
+        if ( (elementalAttack || basicAttack) && Time.realtimeSinceStartup-timeSinceLastShot > 0.5)
         {
             GameObject projectileClone = Instantiate(projectile);
+            if (basicAttack)
+            {
+                projectileClone.GetComponent<Renderer>().material.color = new Color(0,0,0);
+            }
             projectileClone.transform.position = firePoint.position;
             projectileClone.transform.rotation = Quaternion.Euler(0,0, lookAngle);
 
             projectileClone.GetComponent<Rigidbody2D>().velocity = firePoint.right * projectileSpeed;
+            timeSinceLastShot = Time.realtimeSinceStartup;
+
         }
     }
 }

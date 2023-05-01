bfdc884e4c157ce5ecff1942aafd1799caa6791a
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2023-05-01 13:12:13 +0000

    Added firball to player

--- ItsElementary/Assets/Player/Scripts/PlayerShooting.cs
+++ ItsElementary/Assets/Player/Scripts/PlayerShooting.cs
@@ -6,7 +6,7 @@ public class PlayerShooting : MonoBehaviour
 {
     public GameObject projectile;
     public Transform firePoint;
-    public float projectileSpeed = 50;
+    public float projectileSpeed;
     private Vector2 lookDirection;
     private float lookAngle;
     private float timeSinceLastShot;
@@ -21,7 +21,7 @@ public class PlayerShooting : MonoBehaviour
     {
         lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         lookAngle = Mathf.Atan2(lookDirection.y - transform.position.y, lookDirection.x - transform.position.x) * Mathf.Rad2Deg;
-        Debug.Log(lookDirection.x +","+transform.position.x);
+        Vector3 direction = new Vector3 (lookDirection.normalized.x, lookDirection.normalized.y, 0);
         firePoint.rotation = Quaternion.Euler(0, 0, lookAngle);
         bool elementalAttack = Input.GetMouseButton(0);
         bool basicAttack = Input.GetMouseButton(1);
@@ -32,7 +32,7 @@ public class PlayerShooting : MonoBehaviour
             {
                 projectileClone.GetComponent<Renderer>().material.color = new Color(0,0,0);
             }
-            projectileClone.transform.position = firePoint.position;
+            projectileClone.transform.position = firePoint.position + direction;
             projectileClone.transform.rotation = Quaternion.Euler(0,0, lookAngle);
 
             projectileClone.GetComponent<Rigidbody2D>().velocity = firePoint.right * projectileSpeed;

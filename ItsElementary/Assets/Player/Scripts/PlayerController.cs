afa27a2f43356fc5c611af00bcb176b43ebf3068
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2023-05-01 10:28:15 +0000

    player added to sample scene and camera follows player

--- ItsElementary/Assets/Player/Scripts/PlayerController.cs
+++ ItsElementary/Assets/Player/Scripts/PlayerController.cs
@@ -10,15 +10,22 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveDirection;
     private bool elementalAttack;
     private bool basicAttack;
+    public Camera cam;
+    private Vector3 target;
+    private Vector3 playerLastPosition;
+    public float smoothTime = 0.25f;
+
 
     // Update is called once per frame
     void Update()
     {
         ProcessInput();
+        //SetCameraTarget();
     }
 
     void FixedUpdate()
     {
+        SetCameraTarget();
         Move();
         MousePosition();
     }
@@ -35,10 +42,21 @@ public class PlayerController : MonoBehaviour
         basicAttack = Input.GetMouseButton(1);
         
     }
+
+    void SetCameraTarget()
+    {
+        Vector2 movementDelta = transform.position - playerLastPosition;
+        target.x += movementDelta.x;
+        target.y += movementDelta.y;
+        target.z = -10;
+        playerLastPosition = transform.position; 
+    }
     
     void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        cam.transform.position = target;
+
     }
 
     void MousePosition()

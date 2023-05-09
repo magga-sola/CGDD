d94840878bc77075963ddbe0765fa26edcf0a16b
hrefnanamfa <hrefnanamfa@hotmail.com>
2023-05-09 15:16:46 +0000

    More changes for going between levels

--- ItsElementary/Assets/Rooms/Scripts/ExitRoomScript.cs
+++ ItsElementary/Assets/Rooms/Scripts/ExitRoomScript.cs
@@ -5,6 +5,7 @@ public class ExitRoomScript : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D col)
     {
+        Debug.Log("trigger");
         if (col.gameObject.name == "Player")
         {
             GameManager.instance.GoToNextLevel();

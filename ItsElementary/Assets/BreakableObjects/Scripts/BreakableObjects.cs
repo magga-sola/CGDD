96f4eb79aed8a2e80f736447ba7700b8f77b218e
magga-sola <margreta19@ru.is>
2023-05-10 12:23:02 +0000

    initializing breakable objects

--- ItsElementary/Assets/BreakableObjects/Scripts/BreakableObjects.cs
+++ ItsElementary/Assets/BreakableObjects/Scripts/BreakableObjects.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class BreakableObjects : MonoBehaviour
 {
+        public enum Size
+    {
+        Large = 0,
+        Big = 1,
+        Medium = 2,
+        Small = 3,
+        Tiny = 4
+    }
+
+    public Size size;
+    public Sprite[] elementArray;
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] brokenArray;
+    public GameManager.Element element;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer.sprite = elementArray[(int)element + (int)size];
     }
 
     // Update is called once per frame

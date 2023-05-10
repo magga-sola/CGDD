f7e3f341813cfb6e479cde114b217e65a357775b
magga-sola <margreta19@ru.is>
2023-05-10 17:11:23 +0000

    DialoguePanel

---
+++ ItsElementary/Assets/UI/Dialogue/Scripts/DialogueManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    
+
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    // Update is called once per frame
+
+}

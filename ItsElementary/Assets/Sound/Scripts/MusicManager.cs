cefb9137afc73215636f47c87e3d96e5cc0604fa
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2023-05-14 14:40:02 +0000

    Removed loop music

--- ItsElementary/Assets/Sound/Scripts/MusicManager.cs
+++ ItsElementary/Assets/Sound/Scripts/MusicManager.cs
@@ -41,7 +41,6 @@ public class MusicManager : MonoBehaviour
         } else 
         {
             introDone = true;
-            Debug.Log(loopMusic);
             loopMusic.Play();
         }
     }
